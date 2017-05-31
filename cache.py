c5cbfcfee18395cdc6f51d8b6d9ec1503f1d7c64
gianduja <jlagrange@gmail.com>
2017-05-31 22:55:32 +0200

    cleanup

--- cache.py
+++ cache.py
@@ -1,22 +1,22 @@
-import codecs
-
 class Cache:
-	def __init__(self):
-		self.cache = {}
-	# end init
+    def __init__(self):
+        self.cache = {}
 
-	def clear(self):
-		self.cache.clear()
-	#end clear
+    # end init
 
-	def add(self, key, server_id):
-		self.cache[key] = server_id
-	#end add
+    def clear(self):
+        self.cache.clear()
 
-	def get(self, key):
-		try:
-			return self.cache[key]
-		except:
-			return None
-	#end get
-#end Cache
+    # end clear
+
+    def add(self, key, server_id):
+        self.cache[key] = server_id
+
+    # end add
+
+    def get(self, key):
+        return self.cache[key]
+
+        # end get
+
+# end Cache
