0a34ddf871a0f187df5daa6d4a3d06d9cd21bf85
gianduja <jlagrange@gmail.com>
2017-05-31 22:52:00 +0200

    better SIGA search function

---
+++ utils.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+def validateQuotes(text):
+    return text.count('"') % 2 == 0
+
+
+def tokenize(text):
+    if validateQuotes(text):
+        groups = text.split('"')
+        quoted = []
+        non_quoted = []
+        parity = 0
+        for g in groups:
+            if parity:
+                quoted.append(g)
+            else:
+                non_quoted.append(g)
+            parity = 1 - parity
+        # end for
+        quoted = [x for x in quoted if x != '']
+        non_quoted = ' '.join(non_quoted).split()
+
+        quoted.extend(non_quoted)
+        return quoted
+
+    else:
+        return text.replace('"', '').split()
+
+
+def match(text, sl):
+    nsl = list(sl)
+    for token in tokenize(text):
+        nsl = filter(lambda x: re.search(r'\b' + token + r'\b', x['text'], flags=re.I), nsl)
+    return nsl
