12659d03f79c301fd48132ab50f08194b034d01a
gianduja <jlagrange@gmail.com>
2018-06-05 22:27:17 +0200

    :fire: :building_construction: Reset everything to a loop-based python-telegram-bot

--- credentials.py
+++
@@ -1,8 +0,0 @@
-import ConfigParser
-
-config = ConfigParser.ConfigParser()
-config.read('config.ini')
-
-TOKEN = config.get('Credentials', 'TOKEN')
-APP_URL = config.get('Credentials', 'APP_URL')
-OWN_NAME = config.get('Credentials', 'OWN_NAME')
